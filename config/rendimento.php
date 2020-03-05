bc90ae94d6f3fc429fe52f304c799b6a26be8130
danilogocarlos <51349361+danilogocarlos@users.noreply.github.com>
2020-03-05 16:32:21 -0300

    dados pelo env
    pega as informações pelo .env

--- config/rendimento.php
+++ config/rendimento.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'client_id'     => '',
-    'ChaveAcesso'   => '',
-    'autenticacao'  => '',
-    'EnderecoAPI'   => '',
-    'Agencia'       => '',
-    'ContaCorrente' => '',
+    'client_id'     => env('RENDIMENTO_CLIENTE_ID'),
+    'ChaveAcesso'   => env('RENDIMENTO_CHAVE_ACESSO'),
+    'autenticacao'  => env('RENDIMENTO_AUTENTICACAO'),
+    'EnderecoAPI'   => env('RENDIMENTO_ENDERECO'),
+    'Agencia'       => env('RENDIMENTO_AGENCIA'),
+    'ContaCorrente' => env('RENDIMENTO_CONTA'),
 ];
